8a709b2079da0f13400919898175b1ac72c6a666
Ole Henrik Stabell <ole@henrikstabell.com>
2016-12-03 22:52:37 +0100

    removed non-website files

--- main.js
+++
@@ -1,7 +0,0 @@
-var menubar = require('menubar')
-
-var mb = menubar({index:"https://typekit.com/fonts", tooltip:"Typekit Manager"})
-
-mb.on('ready', function ready () {
-  console.log('app is ready')
-})
